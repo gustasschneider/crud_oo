7c3c45a87ec82481140a9fc8eb235b02862efb4d
gustaschneider <gustavo.farias@genesisgroup.com.br>
2021-03-04 18:39:02 -0300

    first commit: Projeto CRUD em PHP 7 sup

---
+++ adicionar.php
@@ -0,0 +1,13 @@
+<script type="text/javascript" src="assets/js/script.js"></script>
+<h1>Adicionar</h1>
+
+<form method="POST" action="adicionar_submit.php">
+	Nome:<br/>
+	<input type="text" name="nome" /><br/><br/>
+
+	E-mail:<br/>
+	<input type="email" name="email" /><br/><br/>
+
+	<input type="submit" value="Adicionar" />
+</form>
+<button id="btn">Fechar</button>
---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(function(){
+
+	$('.modal_ajax').on('click', function(e){
+		e.preventDefault();
+
+		$('.modal').html('Carregando...');
+		$('.modal_bg').show();
+
+		var link = $(this).attr('href');
+
+		$.ajax({
+			url:link,
+			type:'GET',
+			success:function(html){
+				$('.modal').html(html);
+			}
+		});
+
+
+	});
+
+    $('#btn').on('click', function(){
+        $('.modal_bg').hide();
+    })
+
+});
