9d1a489e0f0677f1e67c3be91b3e757c85462af0
gustaschneider <gustavo.farias@genesisgroup.com.br>
2021-03-05 15:08:32 -0300

    add arquivo jquery

--- index.php
+++ index.php
@@ -77,6 +77,7 @@ $contato = new Contato();
         </div>
     </div>
 </div>
+<script type="text/javascript" src="assets/js/jquery-3.6.0.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
