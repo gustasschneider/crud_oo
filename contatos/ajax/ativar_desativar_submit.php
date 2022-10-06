e77e182a33d8eded3804f5da85629fd052cc982b
gustaschneider <gustavo.farias@genesisgroup.com.br>
2022-10-06 13:33:20 -0300

    ajustes em 06/10

--- contatos/ajax/ativar_desativar_submit.php
+++ contatos/ajax/ativar_desativar_submit.php
@@ -3,6 +3,7 @@ include '../../class/Contato.class.php';
 $Contato = new Contato();
 
 $id_contato = $_POST['id_contato'];
+$status_contato = $_POST['status_contato'];
 
-$Contato->ativarContato($id_contato);
+echo $Contato->ativarDesativarContato($id_contato, $status_contato);
 ?>
